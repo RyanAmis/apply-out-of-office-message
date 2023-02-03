35312e2de45cf3ef6dd177125105808c5e01f60a
RyanAmis <118458603+RyanAmis@users.noreply.github.com>
2023-02-03 10:50:47 +0000

    03/02 10:50 - changes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+from exchangelib import DELEGATE, Account, Credentials, Configuration
+
+usrn = input("Username:...")
+pswd = input("Password:...")
+username = usrn
+password = pswd
+Credentials = Credentials(usrn, pswd)
+
+config = Configuration(server='SMTP.landmark.co.uk', credentials=Credentials)
+
+Account = Account(
+    primary_smtp_address=usrn,
+    credentials=Credentials,
+    autodiscover=False,
+    config=config,
+    access_type=DELEGATE,
+)
+
+print(Account)
